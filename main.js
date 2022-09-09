59041ab567ff08411d93552a66e95b9e4784bfb9
Olivia17111 <99030869+Olivia17111@users.noreply.github.com>
2022-09-09 19:22:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function Bedroom() {
+window.location="bedroom.html";
+}
+
+function AC() {
+window.location="ac.html";
+}
+
+function TV() {
+window.location="tv.html";
+}
+
+function Desk() {
+window.location="desk.html";
+}    
+
+function Plants() {
+window.location="plants.html";
+}
+
